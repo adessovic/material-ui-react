8c8376698ca5b7a2c4631d56293b009b9af5c15a
Aleks C <aleksandar.cincarevic@adesso-sweden.se>
2022-10-10 16:50:57 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Button from '@mui/material/Button';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>React with Material UI</p>
+        <Button variant='contained' href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</Button>
       </header>
     </div>
   );
